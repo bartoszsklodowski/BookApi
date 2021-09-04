65179e453ec2a1f77b4b994da349fbe777ffae04
Bartosz Skłodowski <sklodowski94@gmail.com>
2021-09-05 00:03:03 +0200

    Add db path.

--- google_books/urls.py
+++ google_books/urls.py
@@ -1,11 +1,12 @@
 from rest_framework import routers
 from django.urls import path, include
-from .views import BookViewSet
+from .views import BookViewSet, db_create
 
 router = routers.DefaultRouter()
 router.register(r'books', BookViewSet)
 
 urlpatterns = [
+    path('db/', db_create, name='db'),
     path('', include(router.urls)),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework'))
 ]
