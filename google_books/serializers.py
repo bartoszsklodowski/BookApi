2215a13cdf347aa835809cb958c6174d849d246f
Bartosz Skłodowski <sklodowski94@gmail.com>
2021-09-05 00:04:24 +0200

    Add id field.

--- google_books/serializers.py
+++ google_books/serializers.py
@@ -1,7 +1,9 @@
 from .models import Book
 from rest_framework import serializers
 
+
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ['title', 'authors', 'published_date', 'categories', 'average_rating', 'ratings_count', 'thumbnail']
+        fields = ['id', 'title', 'authors', 'published_date', 'categories', 'average_rating', 'ratings_count',
+                  'thumbnail']
