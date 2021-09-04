9b1a6f5d30566aa5d240dbb12a656c0680683c71
Bartosz Skłodowski <sklodowski94@gmail.com>
2021-09-05 00:04:13 +0200

    Add id field.

--- google_books/models.py
+++ google_books/models.py
@@ -3,13 +3,14 @@ from django.db import models
 
 class Book(models.Model):
 
-    title = models.CharField(max_length=50)
-    authors = models.CharField(max_length=50)
-    published_date = models.CharField(max_length=10)
-    categories = models.CharField(max_length=50)
-    average_rating = models.IntegerField()
-    ratings_count = models.IntegerField()
-    thumbnail = models.CharField(max_length=100)
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=50, null=True)
+    authors = models.CharField(max_length=50, null=True)
+    published_date = models.CharField(max_length=10, null=True)
+    categories = models.CharField(max_length=50, null=True)
+    average_rating = models.IntegerField(null=True)
+    ratings_count = models.IntegerField(null=True)
+    thumbnail = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return f"{self.title} {self.authors}"
