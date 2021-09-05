71386b8b98ff6959a24d5ebc85236104b95ae81a
Bartosz Skłodowski <sklodowski94@gmail.com>
2021-09-05 12:34:46 +0200

    Delete comments.

--- google_books/models.py
+++ google_books/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 class Book(models.Model):
-
     id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=50, null=True)
     authors = models.CharField(max_length=50, null=True)
@@ -14,16 +13,3 @@ class Book(models.Model):
 
     def __str__(self):
         return f"{self.title} {self.authors}"
-
-
-# {
-#     "title": "Hobbit czyli Tam i z powrotem",
-#     "authors": ["J. R. R. Tolkien"],
-#     "published_date": "2004",
-#     "categories": [
-#         "Baggins, Bilbo (Fictitious character)"
-#       ],
-#     "average_rating": 5,
-#     "ratings_count": 2,
-#     "thumbnail": "http://books.google.com/books/content?id=YyXoAAAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
-# }
