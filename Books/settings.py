76fcb46d2cbb7bf1ed739d2858a36ef61763cf59
Bartosz Skłodowski <sklodowski94@gmail.com>
2021-09-20 23:17:38 +0200

    Update settings.py. Add decouple module to hide secret key.

--- Books/settings.py
+++ Books/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 import dj_database_url
 import django_heroku
 
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pd*!y*=@+-c1!(xo1ag!i0cszohtru8_x^63u_py0&(^ek8(ui'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
